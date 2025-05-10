e61066995721ad818bb756fc8a3c6e8532c643fe
Evan Chase <chaseea0@gmail.com>
2025-05-10 13:07:06 -0400

    add walls to container

--- rectangle-demo.js
+++ rectangle-demo.js
@@ -1,5 +1,5 @@
 const matterContainer = document.querySelector("#matter-container");
-const groundHeight = 60;
+const shapeThickness = 60;
 
 // module aliases
 let Engine = Matter.Engine,
@@ -24,18 +24,41 @@ let render = Render.create({
     }
 });
 
-// create two boxes and a ground
+// create scene objects
 let boxA = Bodies.rectangle(400, 200, 80, 80);
 let boxB = Bodies.rectangle(450, 50, 80, 80);
 let ground = Bodies.rectangle(
     matterContainer.clientWidth / 2,
-    matterContainer.clientHeight + groundHeight / 2,
+    matterContainer.clientHeight + shapeThickness / 2,
     matterContainer.clientWidth * 2,
-    groundHeight,
-    { isStatic: true });
+    shapeThickness,
+    {
+        isStatic: true
+    }
+);
+
+let leftWall = Bodies.rectangle(
+    0 - shapeThickness / 2,
+    matterContainer.clientHeight / 2,
+    shapeThickness,
+    matterContainer.clientHeight * 5,
+    {
+        isStatic: true
+    }
+);
+
+let rightWall = Bodies.rectangle(
+    matterContainer.clientWidth + shapeThickness / 2,
+    matterContainer.clientHeight / 2,
+    shapeThickness,
+    matterContainer.clientHeight * 5,
+    {
+        isStatic: true
+    }
+);
 
 // add all of the bodies to the world
-Composite.add(engine.world, [boxA, boxB, ground]);
+Composite.add(engine.world, [boxA, boxB, ground, leftWall, rightWall]);
 
 let mouse = Matter.Mouse.create(render.canvas);
 let mouseConstraint = Matter.MouseConstraint.create(engine, {
@@ -69,7 +92,7 @@ function handleResize(matterContainer) {
         ground,
         Matter.Vector.create(
             matterContainer.clientWidth / 2,
-            matterContainer.clientHeight + groundHeight / 2
+            matterContainer.clientHeight + shapeThickness / 2
         )
     )
 }
