86d249928f39a6c1fa8a39c2c0e3b926daa6267f
Evan Chase <chaseea0@gmail.com>
2025-05-10 12:33:23 -0400

    Create index page and import matter CDN
    - create index page
    - add matter.js cdn
    - add boilerplate from matter docs
    - add simple styles
    
    Note: The boilerplate from matter's docs has been modified based on suggestions from the Youtube video "Better Boilerplate for Matter.js (responsive canvas)" by user Web Bae (https://www.youtube.com/watch?v=kdSDTaYY700)

---
+++ rectangle-demo.js
@@ -0,0 +1,47 @@
+const matterContainer = document.querySelector("#matter-container");
+const groundHeight = 60;
+
+// module aliases
+let Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Bodies = Matter.Bodies,
+    Composite = Matter.Composite;
+
+// create an engine
+let engine = Engine.create();
+
+// create a renderer
+let render = Render.create({
+    element: matterContainer,
+    engine: engine,
+    options: {
+        width: matterContainer.clientWidth,
+        height: matterContainer.clientHeight,
+        background: "transparent",
+        wireframes: true,
+        showAngleIndicator: true
+    }
+});
+
+// create two boxes and a ground
+let boxA = Bodies.rectangle(400, 200, 80, 80);
+let boxB = Bodies.rectangle(450, 50, 80, 80);
+let ground = Bodies.rectangle(
+    matterContainer.clientWidth / 2,
+    matterContainer.clientHeight + groundHeight / 2,
+    matterContainer.clientWidth,
+    groundHeight,
+    { isStatic: true });
+
+// add all of the bodies to the world
+Composite.add(engine.world, [boxA, boxB, ground]);
+
+// run the renderer
+Render.run(render);
+
+// create runner
+let runner = Runner.create();
+
+// run the engine
+Runner.run(runner, engine);
